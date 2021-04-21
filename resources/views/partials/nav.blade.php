bb12c7cf742a20f7454aaa7bd7eb481056e899c3
Asklat <alejandro.gamaza.martinez.vega@gmail.com>
2021-04-21 18:08:11 +0200

    estilos boostrap

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,10 +1,23 @@
-<nav>
-    <ul>
-        <li class="{{ setActive('about') }}"><a href="{{route('about')}}">Nuesto clan</a></li>
-        <li class="{{ setActive('news.index') }}"><a href="{{route('news.index')}}">Noticias</a></li>
-        <li class="{{ setActive('actualizaciones') }}"><a href="{{route('actualizaciones')}}">Actualizaciones</a></li>
-        <li class="{{ setActive('solicitud') }}"><a href="{{route('solicitud')}}">Solicitud</a></li>
-    </ul>
+<nav class="navbar navbar-light navbar-expand-lg bg-white shadow-sm">
+    <div class="container">
+        <button class="navbar-toggler" type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="{{ __('Toggle navigation') }}">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a class="nav-link {{ setActive('about') }}" href="{{route('about')}}">Nuesto clan</a></li>
+            <li class="nav-item"><a class="nav-link {{ setActive('news.index') }}"  href="{{route('news.index')}}">Noticias</a></li>
+            <li class="nav-item"><a class="nav-link {{ setActive('actualizaciones') }}"  href="{{route('actualizaciones')}}">Actualizaciones</a></li>
+            <li class="nav-item"><a class="nav-link {{ setActive('solicitud') }}"  href="{{route('solicitud')}}">Solicitud</a></li>
+        </ul>
+    </div>
+    </div>
 
     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
         @csrf
